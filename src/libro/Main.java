07dcf4a02e4a648a1fda28ec1aec7eeee529981d
fernando <Fernando.Martinez22@alu.uclm.es>
2022-04-20 20:36:33 +0200

    v2

---
+++ src/libro/Main.java
@@ -0,0 +1,14 @@
+package libro;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int a=1;
+        for (int i = 0; i < 10; i++) {
+            a+= i;
+        }
+    }
+}
